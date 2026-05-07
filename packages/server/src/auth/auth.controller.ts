426d17c107ce3694384ae75f5f4b6cc176a7ec76
barrera77 <barrera_ml@hotmail.com>
2026-05-07 00:26:57 -0600

    fix(auth) increase cookie expiration

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax',
-        maxAge: 100 * 60 * 10,
+        maxAge: 1000 * 60 * 10,
       });
       return { requiresTwoFactorSetup: true, message: '2FA setup required' };
     }
